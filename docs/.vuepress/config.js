cc9638d8676e75e4b21d90a2a9867375471dfaf3
123456 <hoochanlon@outlook.com>
2023-08-27 17:41:12 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -59,7 +59,8 @@ module.exports = {
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
                     ['/zufang-note/fxxkzf','租房防坑记录（2019）'],
-                    ['/zufang-note/zufangxinzhi','龙岗坂田租房新知（2022）']
+                    ['/zufang-note/zufangxinzhi','龙岗坂田租房新知（2022）'],
+                    ['/zufang-note/shiyouhuigu.md','再租房经历与室友回顾(2023)']
                 ]
               }
             ]
