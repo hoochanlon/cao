dd6c47c96f60011cfa0d094a93e33bc65c94be16
Hu TsingLoong <hoochanlon@outlook.com>
2023-09-27 15:35:58 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,8 +45,7 @@ module.exports = {
                   ['/hr/hr-and-candidate','人力资源与应聘者（2020）'],
                   ['/hr/qizha','高校毕业生就业形势分析及对策（2023）'],
                   ['/hr/mianshixijie','面试过程中需要关注的（2023）'],
-                ['/hr/sannian-hou-mianshi','工作三年脱离人才市场的教训（2023）'],
-                ['/hr/jhrzcwdszz','在企业三年被辞退的经历（2023）']
+                ['/hr/sannian-hou-mianshi','工作三年脱离人才市场的教训（2023）']
                 ]
               },
               {
@@ -57,7 +56,8 @@ module.exports = {
                   ['/companyofchina/the-factory-trap','流水线工厂陷阱（2019）'],
                   ['/companyofchina/jiegou-it-waibao','解构非规范型服务企业（2020）'],
                   ['/companyofchina/caiyuan','企业裁员研究（2023）'],
-                 ['/companyofchina/fxxkchina','公司概况·老员工·我个人境遇（2023）']
+                  ['/companyofchina/fxxkchina','公司概况·老员工·我个人境遇（2023）'],
+                  ['/hr/jhrzcwdszz','在企业三年被辞退的经历（2023）']
                 ]
               },
               {
