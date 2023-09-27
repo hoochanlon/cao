77998b62c1a0bfd7fdf2a0dc06301c4de62b1897
Hu TsingLoong <hoochanlon@outlook.com>
2023-09-27 15:13:12 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,4 @@
 // .vuepress/config.js
-import { defineUserConfig } from "@vuepress/cli";
-import { defaultTheme } from "@vuepress/theme-default";
-import { cut } from "nodejs-jieba";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
-
-const base = <"/" | `/${string}/`>process.env["BASE"] || "/";
-
-export default defineUserConfig({
-  base,
-  title: "Search Pro",
-  description: "VuePress Client Search plugin",
-
-  plugins: [
-    searchProPlugin({
-      indexContent: true,
-      hotReload: true,
-      customFields: [
-        {
-          getter: ({ frontmatter }) => frontmatter.tag as string[],
-          formatter: `Tag: $content`,
-        },
-      ],
-      indexOptions: {
-        tokenize: (text, fieldName) =>
-          fieldName === "id" ? [text] : cut(text, true),
-      },
-    }),
-  ],
-});
 
 module.exports = {
     title: '《草》',
